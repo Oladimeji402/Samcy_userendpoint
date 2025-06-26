710f6ce2887561934521b4990a9b89a058160b38
oladimeji402 <olamoney665@gmail.com>
2025-06-26 13:34:27 +0100

    updated version

--- register.php
+++ register.php
@@ -1,7 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
 header("Content-Type: application/json");
 require_once 'config/database.php';
 require_once 'includes/functions.php';
+
+
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
